f13d5740979223a3a821417b61edef14b571b022
chicobanza13 <fmelo1168@gmail.com>
2026-05-18 15:24:39 +0100

    Change database password for PostgreSQL settings
    Updated PostgreSQL database configuration for production.

--- config/settings.py
+++ config/settings.py
@@ -82,22 +82,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
- 
-
-# Para usar PostgreSQL em produção, comente acima e descomente:
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'estagio',   
         'USER': 'postgres',
-        'PASSWORD': 'joao',
+        'PASSWORD': 'atuapassword',
         'HOST': 'localhost',
         'PORT': '5432',
     }
