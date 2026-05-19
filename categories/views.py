83986cc56e032e4d582b4911dabbfdf728d32c54
joao <pereiragui2008@gmail.com>
2026-05-19 12:29:02 +0100

    alterei o views.py/categories

--- categories/views.py
+++ categories/views.py
@@ -1,10 +1,20 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
 from .models import Category
 from .serializers import CategorySerializer
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user and request.user.is_staff
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset           = Category.objects.all()
     serializer_class   = CategorySerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminOrReadOnly]
     lookup_field       = "slug"
+    filter_backends    = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields      = ["name", "description"]
+    ordering_fields    = ["name", "created_at"]
