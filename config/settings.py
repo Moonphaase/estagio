df08683ca0398951ca0badac20ce03aded97e607
joao <pereiragui2008@gmail.com>
2026-05-19 12:35:46 +0100

    config ta feito

--- config/settings.py
+++ config/settings.py
@@ -61,7 +61,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'estagio',
         'USER': 'postgres',
-        'PASSWORD': 'chico',
+        'PASSWORD': 'joao',
         'HOST': 'localhost',
         'PORT': '5432',
     }
