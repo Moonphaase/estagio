c8936687b496fdbb30ce88d362a93b8a2fa47882
chicobanza13 <fmelo1168@gmail.com>
2026-05-19 12:08:31 +0100

    profile editar cns

--- config/settings.py
+++ config/settings.py
@@ -61,7 +61,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'estagio',
         'USER': 'postgres',
-        'PASSWORD': 'joao',
+        'PASSWORD': 'chico',
         'HOST': 'localhost',
         'PORT': '5432',
     }
