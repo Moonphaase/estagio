4cb88f2c7ee5169ad5faa7a643cc4fbcce77f66f
marco <baratamarcoandre@gmail.com>
2026-05-26 16:19:58 +0100

    69

--- config/settings.py
+++ config/settings.py
@@ -63,7 +63,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'estagio',
         'USER': 'postgres',
-        'PASSWORD': 'joao',
+        'PASSWORD': 'marco',
         'HOST': 'localhost',
         'PORT': '5432',
     }
