d98ed42c07141920307448eebcfa4da3bf3f4562
joao <pereiragui2008@gmail.com>
2026-07-06 15:02:12 +0100

    a

--- config/settings.py
+++ config/settings.py
@@ -150,13 +150,15 @@ else:
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'api_keys.authentication.APIKeyAuthentication', # Prioridade máxima
+        'api_keys.authentication.APIKeyAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated', # Ninguém entra sem ser autenticado
+        # ISTO OBRIGA TODAS AS VIEWS A EXIGIREM AUTENTICAÇÃO
+        'rest_framework.permissions.IsAuthenticated', 
     ),
+
     'DEFAULT_PAGINATION_CLASS': 'core.pagination.StandardPagination',
     'PAGE_SIZE': 10,
     'EXCEPTION_HANDLER': 'core.exceptions.custom_exception_handler',
