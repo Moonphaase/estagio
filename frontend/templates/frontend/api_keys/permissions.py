871ff901ce144caf64ce020df9726416d06cd2d2
joao <pereiragui2008@gmail.com>
2026-07-08 09:43:13 +0100

    a

--- frontend/templates/frontend/api_keys/permissions.py
+++ frontend/templates/frontend/api_keys/permissions.py
@@ -1,5 +1,5 @@
 from rest_framework.permissions import BasePermission
-from api_keys.models import ApiKey  # ajusta o nome do modelo se for diferente
+from datasets.models import ApiKey  # ajusta o nome do modelo se for diferente
 
 
 class HasValidApiKey(BasePermission):
